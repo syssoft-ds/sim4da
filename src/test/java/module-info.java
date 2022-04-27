51fcaed6584fafd6287d67cd527f9b5c61d9d2b0
Peter Sturm <sturm@uni-trier.de>
2022-04-27 17:51:41 +0200

    Ups

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 open module dev.oxoo2a.sim4da.test {
-=======
-open module dev.oxoo2a.sim4da .test {
->>>>>>> junit-issue
     requires com.google.gson;
 
     requires transitive org.junit.jupiter.engine;
