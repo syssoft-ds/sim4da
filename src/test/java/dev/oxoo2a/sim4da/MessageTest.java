d0243efaad61e55fd1ac6ad8c201bf1851c05c83
Peter Sturm <sturm@uni-trier.de>
2022-04-29 15:55:07 +0200

    More tests

--- src/test/java/dev/oxoo2a/sim4da/MessageTest.java
+++ src/test/java/dev/oxoo2a/sim4da/MessageTest.java
@@ -2,12 +2,28 @@ package dev.oxoo2a.sim4da;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 public class MessageTest {
+	@BeforeAll
+	public static void createMessage () {
+		m = new Message().add("a","b").add("c","d");
+	}
+
+	private static Message m;
 
 	@Test
 	public void testMessageBasics () {
-		Message m = new Message().add("a","b");
 		assertEquals(m.query("a"), "b");
+		assertEquals(m.query("c"), "d");
+		assertEquals(m.query("no_key"),null);
+	}
+
+	@Test
+	public void serializeAndDeserialize () {
+		String m_json = m.toJson();
+		Message m2 = Message.fromJson(m_json);
+		assertEquals(m2.query("a"),"b");
+		assertEquals(m2.query("c"),"d");
 	}
 }
