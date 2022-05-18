94764d366897829797c9e012f46969c4753097e7
janschu99 <105646877+janschu99@users.noreply.github.com>
2022-05-18 15:57:23 +0200

    Added implementation of logical timestamps
    Currently, Extended Lamport Time and Vector time are available to choose from, but the implementation is designed in a way that other clock/timestamp concepts could be added in the future.

--- src/main/java/dev/oxoo2a/sim4da/Message.java
+++ src/main/java/dev/oxoo2a/sim4da/Message.java
@@ -5,14 +5,12 @@ public class Message {
     private final int senderId;
     private final int receiverId;
     private final MessageType type;
-    private final LogicalTimestamp timestamp;
     private final String payload;
     
-    public Message(int senderId, int receiverId, MessageType type, LogicalTimestamp timestamp, String payload) {
+    public Message(int senderId, int receiverId, MessageType type, String payload) {
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.type = type;
-        this.timestamp = timestamp;
         this.payload = payload;
     }
     
@@ -28,10 +26,6 @@ public class Message {
         return type;
     }
     
-    public LogicalTimestamp getTimestamp() {
-        return timestamp;
-    }
-    
     public String getPayload() {
         return payload;
     }
@@ -39,8 +33,7 @@ public class Message {
     @Override
     public String toString() {
         return "Message(sender="+senderId+",receiver="+receiverId+","
-                +(type==MessageType.BROADCAST ? "Broadcast" : "Unicast")+",timestamp="+timestamp
-                +",payload=<"+payload+">)";
+                +(type==MessageType.BROADCAST ? "Broadcast" : "Unicast")+",payload=<"+payload+">)";
     }
     
     public enum MessageType {
