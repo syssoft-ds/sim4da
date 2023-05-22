ca1c1d9182547a3b00c3f5e83ff2def9cec7a51a
Maximilian Späth <s4mnspae@uni-trier.de>
2023-05-22 12:24:22 +0200

    lamportTime and Vector Time done

--- src/main/java/dev/oxoo2a/sim4da/Main.java
+++ src/main/java/dev/oxoo2a/sim4da/Main.java
@@ -1,25 +1,48 @@
 package dev.oxoo2a.sim4da;
 
+import dev.oxoo2a.sim4da.logicalClocks.LamportClockNode;
+import dev.oxoo2a.sim4da.logicalClocks.VectorClockNode;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
         int n_nodes = 5;
-        TokenRingNode.useLamportClock=false;
-        TokenRingNode.useVectorTime=true;
-        if(TokenRingNode.useVectorTime)
-            TokenRingNode.knowledgeOfTheTotalAmountOfNodes=n_nodes;
+
+        ClockTypeToUse clockTypeToUse = ClockTypeToUse.lamportClock;
+
         Simulator s = Simulator.createDefaultSimulator(n_nodes);
-        for (int id=0; id<n_nodes; id++) {
+        /*for (int id=0; id<n_nodes; id++) {
             Node n = new TokenRingNode(id);
             s.attachNode(id,n);
-        }
-        try{
+        }*/
 
-            s.runSimulation(1000000000);
+        switch (clockTypeToUse){
+            case lamportClock -> {
+                for (int id=0; id<n_nodes; id++) {
+                    Node n = new LamportClockNode(id);
+                    s.attachNode(id,n);
+                }
+            }
+            case vectorClock -> {
+                for (int id=0; id<n_nodes; id++) {
+                    Node n = new VectorClockNode(id,n_nodes);
+                    s.attachNode(id,n);
+                }
+            }
+        }
+
+
+
+        try{
+            s.runSimulation(5);
         }catch (InstantiationException e){
             System.err.println("Instantiation failed. Time to investigate.");
         }
+    }
+    private enum ClockTypeToUse{
+        lamportClock,
+        vectorClock
     }
 }
